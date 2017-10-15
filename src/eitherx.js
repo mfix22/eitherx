d0dd0f4bd0ccdde47f8b83065fdd39d13b03b0c7
Mike Fix <mrfix84@gmail.com>
2017-10-15 13:02:48 -0700

    Initial Commit

---
+++ src/eitherx.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const CATCH = 'handleErrors'
+
+class Eitherx extends React.Component {
+  constructor(props) {
+    super(props);
+    if (props.render) {
+      if (!props[CATCH]) {
+        throw new Error(`If \`render\` prop is passed, you must also pass a \`${CATCH}\` callback`)
+      }
+      if (typeof props.render !== 'function' || typeof props[CATCH] !== 'function') {
+        throw new Error(`\render\` and \`${CATCH}\` must both be functions`)
+      }
+    } else {
+      const childCount = React.Children.count(props.children)
+      if (childCount < 1 || childCount > 2) {
+        throw new Error(`Eitherx expects either 1 or 2 children. You passed ${childCount} children`)
+      }
+    }
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    // Display fallback UI
+    this.setState({ hasError: { error, info } });
+  }
+
+  render() {
+    // Render Prop Style
+    if (this.props.render) {
+      return this.state.hasError
+        ? this.props[CATCH](this.state.hasError)
+        : this.props.render()
+    }
+
+    // Wrapper Component Style
+    const [renderChild, errorChild] = React.Children.toArray(this.props.children)
+
+    if (this.state.hasError) {
+      if (!errorChild) return null;
+      return errorChild
+    }
+    return renderChild
+  }
+}
+
+export default Eitherx
---
+++ test/index.spec.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import Eitherx from '../src'
+import renderer from 'react-test-renderer';
+
+test('should throw if < 1 or > 2 children are passed', () => {
+  expect(() => renderer.create(<Eitherx></Eitherx>))
+    .toThrowErrorMatchingSnapshot()
+  expect(() => renderer.create(<Eitherx><div/><div/><div/></Eitherx>))
+    .toThrowErrorMatchingSnapshot()
+})
+
+test('should throw render is passed but not handleErrors', () => {
+  expect(() => renderer.create(
+    <Eitherx
+      render={() => (<div/>)}
+    />
+  )).toThrowErrorMatchingSnapshot()
+})
+
+test('should throw if render or handleErrors is not a function', () => {
+  expect(() => renderer.create(
+    <Eitherx
+      render={() => (<div/>)}
+      handleErrors={"Not a function"}
+    />
+  )).toThrowErrorMatchingSnapshot()
+
+  expect(() => renderer.create(
+    <Eitherx
+      render={"Not a function"}
+      handleErrors={() => (<div/>)}
+    />
+  )).toThrowErrorMatchingSnapshot()
+})
+
+class ErrorComponent extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    throw new Error('Test Error')
+  }
+}
+
+test('should return null if no error handler is passed, and error is thrown', () => {
+  let component = renderer.create(
+    <Eitherx>
+      <p>My Paragraph</p>
+    </Eitherx>
+  )
+  expect(component.toJSON()).toMatchSnapshot();
+
+  component = renderer.create(
+    <Eitherx>
+      <ErrorComponent>"My Error"</ErrorComponent>
+    </Eitherx>
+  )
+  expect(component.toJSON()).toEqual(null);
+})
+
+test('should render second component on error', () => {
+  let component = renderer.create(
+    <Eitherx>
+      <p>"Happy Child 😄"</p>
+      <p>"Error as child ☹️"</p>
+    </Eitherx>
+  )
+  expect(component.toJSON()).toMatchSnapshot();
+
+  component = renderer.create(
+    <Eitherx>
+      <ErrorComponent>"Function as child 😄"</ErrorComponent>
+      <p>"Error as child ☹️"</p>
+    </Eitherx>
+  )
+  expect(component.toJSON()).toMatchSnapshot();
+})
+
+test('should work with render-catch props as well', () => {
+  let component = renderer.create(
+    <Eitherx
+      render={() => (<p>"Render Prop"</p>)}
+      handleErrors={() => (<p>"Catch Prop"</p>)}
+    />
+  )
+  expect(component.toJSON()).toMatchSnapshot();
+
+  component = renderer.create(
+    <Eitherx
+      render={() => (<ErrorComponent>"Render Prop"</ErrorComponent>)}
+      handleErrors={() => (<p>"Catch Prop"</p>)}
+    />
+  )
+  expect(component.toJSON()).toMatchSnapshot();
+})
